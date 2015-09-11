9fd8c681c630c7d542009e02d79807f3afd56c14
Balatzar <bataprod@gmail.com>
2015-09-11 18:58:20 +0200

    cloud et sol

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    
+    
+    function cloud() {
+        $('.cloud').animate({
+            top: "23%"
+        }, 1800);
+        $('.cloud').animate({
+            top: "24%"
+        }, 1800);
+        cloud();
+    };
+    
+    cloud();
+    
+});
