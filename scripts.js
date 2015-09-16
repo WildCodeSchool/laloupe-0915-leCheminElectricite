13ed017f2833b78c964e44e0fad1f2a2abb8d147
Laurent Berthier <proberthier@gmail.com>
2015-09-16 15:29:36 +0200

    first modal on click -> .swing

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-    
+
     $('#fullpage').fullpage({
         anchors:['startPage', 'mainPath'],
     });
-    
+
     var slide = 1;
-    
+
     $(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: slide--;
-        break;
+        switch(e.which) {
+            case 37: slide--;
+                break;
 
-        case 39: slide++;
-        break;
+            case 39: slide++;
+                break;
 
-        default: return; // exit this handler for other keys
-    }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
-});
-    
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
     function cloud() {
         $('.cloud').animate({
             top: "23%"
@@ -28,17 +28,17 @@ $(document).ready(function() {
         }, 1800);
         cloud();
     };
-    
+
     function rotor() {
         $('.rotor-1').transition({ rotate: '60000000deg' }, 7500000000, 'linear');
     };
-    
+
     function rotor2() {
         $('.rotor-2').transition({ rotate: '60000000deg' }, 5600000000, 'linear');
     };
-    
+
     var x = 1;
-    
+
     rotor();
     rotor2();
     cloud();
