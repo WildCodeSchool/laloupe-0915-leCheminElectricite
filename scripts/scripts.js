f3bd8018475c8f8c6ea98b12c129f347fc5e0fda
Laurent Berthier <proberthier@gmail.com>
2015-09-18 16:17:57 +0200

    ajout d'un effet de transition entre le slide 0 et 1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,7 @@
-var firtTime = true;
-
 $(document).ready(function() {
-   
-   $('#fullpage').fullpage({
+
+    var firtTime = true;
+    $('#fullpage').fullpage({
         anchors:['startPage', 'mainPath'],
         loopHorizontal: false,
 
@@ -16,45 +15,54 @@ $(document).ready(function() {
                 firtTime = false;
             }
         },
+        afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex){
+            var loadedSlide = $(this);
+
+            //first slide of the second section
+            if(anchorLink == 'mainPath' && slideIndex == 1){
+                $(".upanim").css('animation', 'upanim 2s ease');
+                $(".upanim").css('opacity', '1');
+            }
+        },
 
     });
-   
-   var slide = 1;
-   
-   $(document).keydown(function(e) {
-   switch(e.which) {
-       case 37: slide--;
-       break;
 
-       case 39: slide++;
-       break;
+    var slide = 1;
 
-       default: return; // exit this handler for other keys
-   }
-   e.preventDefault(); // prevent the default action (scroll / move caret)
-});
-   
-   function cloud() {
-       $('.cloud').animate({
-           top: "23%"
-       }, 1800);
-       $('.cloud').animate({
-           top: "24%"
-       }, 1800);
-       cloud();
-   };
-   
-   function rotor() {
-       $('.rotor-1').transition({ rotate: '60000000deg' }, 7500000000, 'linear');
-   };
-   
-   function rotor2() {
-       $('.rotor-2').transition({ rotate: '60000000deg' }, 5600000000, 'linear');
-   };
-   
-   var x = 1;
-   
-   rotor();
-   rotor2();
-   cloud();
+    $(document).keydown(function(e) {
+        switch(e.which) {
+            case 37: slide--;
+                break;
+
+            case 39: slide++;
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
+    function cloud() {
+        $('.cloud').animate({
+            top: "23%"
+        }, 1800);
+        $('.cloud').animate({
+            top: "24%"
+        }, 1800);
+        cloud();
+    };
+
+    function rotor() {
+        $('.rotor-1').transition({ rotate: '60000000deg' }, 7500000000, 'linear');
+    };
+
+    function rotor2() {
+        $('.rotor-2').transition({ rotate: '60000000deg' }, 5600000000, 'linear');
+    };
+
+    var x = 1;
+
+    rotor();
+    rotor2();
+    cloud();
 });
