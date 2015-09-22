360283ab4e2a3bf16f4cb57cbaba97041210bad7
Atakikoo <atakikoo@gmail.com>
2015-09-22 16:09:45 +0200

    modal battle

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -197,10 +197,22 @@ $(document).ready(function() {
         goGoGo(1, slide);
     });
     
+    $('.animShake').hover(function() {
+        $(this).addClass('animated shake');
+    }, function() {
+        $(this).removeClass('animated shake');
+    });
+
     $('.animPulse').hover(function() {
         $(this).addClass('animated pulse');
     }, function() {
         $(this).removeClass('animated pulse');
+    });
+
+    $('.animBounce').hover(function() {
+        $(this).addClass('animated bounce');
+    }, function() {
+        $(this).removeClass('animated bounce');
     });
     
 });
