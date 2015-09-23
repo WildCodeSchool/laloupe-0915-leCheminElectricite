f8cfa6d372b09667395129f8b0b10bf13eb9b8ae
Balatzar <bataprod@gmail.com>
2015-09-23 10:00:55 +0200

    annotation du js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+//fonction detectant l'url et la rechargeant quand
+//elle n'est pas 'propre'
 window.onload =  function() {
     var str = self.location.href;
     if ((str[str.length - 1]) != '?') {
@@ -6,37 +8,57 @@ window.onload =  function() {
 };
 
 $(document).ready(function() {
-
-    var firstTime = true;
     
+    // /!\/!\/!\/!\/!\/!\
+    //ne pas oublier de mettre a true avant de livre
+    var firstTime = false;
+    
+    //lance l'animation des eoliennes
+    rotor();
+    rotor2();
+    
+    //parametrage de fullpage et methodes associees
     $('#fullpage').fullpage({
         anchors:['startPage', 'mainPath', 'video'],
         loopHorizontal: false,
         animateAnchor: false,
         
+        //lancement de fonctions apres le load de
+        //certaines sections
         afterLoad: function(anchorLink, index){
+            //stock la section actuelle
             var loadedSection = $(this);
-
+            
+            //quand on arrive dans la premiere section
+            //pour la premiere fois
             if(anchorLink == 'mainPath' && firstTime == true) {
+                //rebuild pout enlever la barre blanche
                 $.fn.fullpage.reBuild();
+                //lance le modal de bienvenue
                 $('.firstTime').trigger('click');
                 firstTime = false;
             }
         },
+        //lancement de fonctions apres le load de
+        //certains slide
         afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex){
+            //stock le slide actuel
             var loadedSlide = $(this);
 
+            //quand on est dans la deuxieme section et 
+            //sur le deuxieme slide
             if(anchorLink == 'mainPath' && slideIndex == 1){
+                //apparition du sol progressive
                 $(".upanim").css('animation', 'upanim 2s ease');
                 $(".upanim").css('opacity', '1');
             }
         },
     });
    
-   var slide = 1;
-    
-    goGoGo(1, slide);
+    //stockage du slide actuel
+    var slide = 1;
 
+    //fonctions animant les eoliennes
     function rotor() {
         $('.rotor-1').transition({ rotate: '60000000deg' }, 7500000000, 'linear');
     };
@@ -45,10 +67,12 @@ $(document).ready(function() {
         $('.rotor-2').transition({ rotate: '60000000deg' }, 5600000000, 'linear');
     };
     
-    var animLock = false;
+    //variable definissant la vitesse d'animation
     var speed = 800
     
-    function moveCloud(slide) {
+    //mega fonction pour toutes les animations
+    //hors fullscreen
+    function moveThings(slide) {
         switch(slide) {
             case 1:
                 $('.cloud-one').animate({
@@ -135,13 +159,11 @@ $(document).ready(function() {
         }
     }
     
-    function goGoGo(direction, slide) {
-        moveCloud(slide);
-        if (slide == 1) {
-            rotor();
-            rotor2();
-        }
-    };
+    animLock = false;
+    
+    moveThings(1);
+    
+    //lance les animations avec les touches du clavier
     $(document).keydown(function(e) {
         if (animLock) { return; }
         animLock = true;
@@ -150,14 +172,14 @@ $(document).ready(function() {
             case 37:          //gauche
                 if (slide != 1) {
                     slide--;
-                    goGoGo(0, slide);
+                    moveThings(slide);
                 }
             break;
 
             case 39:          //droite
                 if (slide != 6) {
                     slide++;
-                    goGoGo(1, slide);
+                    moveThings(slide);
                 }
             break;
 
@@ -165,6 +187,8 @@ $(document).ready(function() {
         }
     });
     
+    //lance les animations avec les clicks
+    //sur les fleches
     $('.fp-prev').click(function() {
         if (slide != 1) {
             slide--;
@@ -179,6 +203,8 @@ $(document).ready(function() {
         }
     });
     
+    //lance les animations correspondant
+    //au slide cliqué
     $('#production').click(function() {
         slide = 2;
         goGoGo(1, slide);
@@ -204,6 +230,7 @@ $(document).ready(function() {
         goGoGo(1, slide);
     });
     
+    //animations hover
     $('.animShake').hover(function() {
         $(this).addClass('animated shake');
     }, function() {
